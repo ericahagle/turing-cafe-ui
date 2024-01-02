6477fd55ac28bdc31b9ff1eec142fdfcf2e1c7e2
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-02 08:59:59 -0800

    Render dummy Reservations component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+import Reservations from '../Reservations/Reservations';
 
 function App() {
   const [ reservations, setReservations ] = useState([]);
 
   return (
-    <div className="App">
+    <main className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
       </div>
       <div className='resy-container'>
+        <Reservations />
       </div>
-    </div>
+    </main>
   );
 }
 
---
+++ src/components/Reservations/Reservations.js
@@ -0,0 +1,9 @@
+import './Reservations.css';
+
+function Reservations() {
+  return (
+    <h2>Reservations will appear here</h2>
+  );
+};
+
+export default Reservations;
